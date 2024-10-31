3c8331d81f4250ba8b57bc007ce054fc3acd9c3f
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-31 23:25:33 +0500

    more

--- comma_seperator.py
+++ comma_seperator.py
@@ -1,7 +1,14 @@
+# with open("numbers.txt", "r") as rf:
+#     with open("hello.txt", "w") as wf:
+#         for i in rf.readlines():
+#             wf.write(i.replace("\n", ","))
+
+
 with open("numbers.txt", "r") as rf:
     with open("hello.txt", "w") as wf:
         for i in rf.readlines():
-            wf.write(i.replace("\n", ","))
+            j = i.split("; ")[1]
+            wf.write(j.replace("\n", ","))
 
         
         
